820b68ccce3839baf5d3b7dcfb7d8e805beb0fa9
sanni <43077388+sannidhishukla@users.noreply.github.com>
2025-05-22 10:04:46 -0600

    Install and set up Storybook for documentation (#1)

---
+++ .storybook/main.ts
@@ -0,0 +1,15 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  framework: "@storybook/react-vite",
+  stories: ["../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-essentials", "@whitespace/storybook-addon-html"],
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  docs: {
+    autodocs: true,
+  },
+};
+
+export default config;
---
+++ .storybook/preview.ts
@@ -0,0 +1,14 @@
+import "@newjersey/njwds/dist/css/styles.css";
+
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
