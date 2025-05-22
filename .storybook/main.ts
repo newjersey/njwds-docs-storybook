5713db6a137a49f8be7813e1ce7932c9b6ef443e
Sannidhi Shukla <sannidhi@innovation.nj.gov>
2025-05-22 08:33:28 -0600

    remove js/jsx support and related files

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   framework: "@storybook/react-vite",
-  stories: ["../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../stories/**/*.stories.@(ts|tsx)"],
   addons: ["@storybook/addon-essentials", "@whitespace/storybook-addon-html"],
   core: {
     builder: "@storybook/builder-vite",
