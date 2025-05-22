1c57a5443a2d1b9686d959d7ce47f223fbbc8ba2
sanni <43077388+sannidhishukla@users.noreply.github.com>
2025-05-22 10:28:55 -0600

    Clean up packages + README

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,7 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   framework: "@storybook/react-vite",
   stories: ["../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-essentials", "@whitespace/storybook-addon-html"],
+  addons: ["@storybook/addon-essentials"],
   core: {
     builder: "@storybook/builder-vite",
   },
