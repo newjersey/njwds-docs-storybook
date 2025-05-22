378245910aab83d645e8efd8a1238c6a40485492
Sannidhi Shukla <sannidhi@innovation.nj.gov>
2025-05-22 09:58:48 -0600

    add back button built-in story to test publish

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   framework: "@storybook/react-vite",
-  stories: ["../stories/**/*.stories.@(ts|tsx)"],
+  stories: ["../stories/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-essentials", "@whitespace/storybook-addon-html"],
   core: {
     builder: "@storybook/builder-vite",
