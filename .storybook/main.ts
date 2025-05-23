f4e5db16ed1ecc0fe31c931405c72054624110db
sanni <43077388+sannidhishukla@users.noreply.github.com>
2025-05-23 13:04:44 -0600

    Add interactive example in Storybook for validation on submit that produces a single error (#3)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   framework: "@storybook/react-vite",
-  stories: ["../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../stories/**/*.stories.@(ts|tsx)"],
   addons: ["@storybook/addon-essentials"],
   core: {
     builder: "@storybook/builder-vite",
